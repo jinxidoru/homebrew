7b31f8b69c4b9b9bf7f1e2da8b04ed5f92d08e01
Michael Paul Bailey <jinxidoru@gmail.com>
2011-09-28 12:55:34 -0600

    added oberon-deps formula which maintains the dependencies for oberon

---
+++ Library/Formula/oberon-deps.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+# This formula exists simply to collect the dependencies for oberon
+
+class OberonDeps < Formula
+  url 'https://github.com/rupa/z/tarball/v1.1'
+  md5 '75125fba5be0638dafd2518489d2faf2'
+  version '1'
+
+  depends_on 'ice'
+  depends_on 'mysql++'
+  depends_on 'boost'
+
+  def install
+  end
+
+end
+
+
